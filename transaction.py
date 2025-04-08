bd4d9dd9b91e375ed0d72a39b644dede4b88d889
Ansh Lohani <lohaniansh23@gmail.com>
2025-04-08 13:53:01 +0530

    Created Class Methods withdraw() and deposit()

--- transaction.py
+++ transaction.py
@@ -2,15 +2,30 @@ from account import Account
 from customer import Customer
 
 class Transaction:
-    def __init__(self,senderID,receiverID,amount):
+
+    def __init__(self,senderID,receiverAccountNumber,amount):
         self.sender = senderID
-        self.receiver = receiverID
+        self.receiver = receiverAccountNumber
         self.amount = amount
         Account.withdraw(senderID,amount)
-        Account.deposit(receiverID,amount)
+        Account.deposit(receiverAccountNumber,amount)
 
         with open('data/transaction_logs.txt','a') as f:
             f.write(f"{self.sender} {self.receiver} {self.amount} \n")
 
         print(f"{self.sender} sent {self.amount} to {self.receiver}")
+
+    @classmethod
+    def withdraw(SenderID,amount):
+        with open('data/accounts.txt','r+w') as file:
+            x = file.readlines()
+            for i in x:
+                if f"{SenderID}" in i:
+                    ...
+                else:
+                    ...
+
+    @classmethod 
+    def deposit(receiverAccNo,amount):
+        ...
 
